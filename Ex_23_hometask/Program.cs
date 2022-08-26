6703617ad7cf6c071f0baf1048e7a379d922ab4b
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-26 12:29:03 +0300

    Ex23 hometask

---
+++ Ex_23_hometask/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимае число N и выдает
+// таблицу кубов числе от 1 до N
+
+void CoubOfN (int n)
+{
+    Console.WriteLine($"Таблица кубов от 1 до {n}:");
+
+    int i = 0;
+    while(i <= n)
+    {
+        double help = Math.Pow(i, 3);
+        Console.Write(help + " ");
+        i++;
+    }
+
+}
+
+Console.WriteLine("Введите ваше число N?");
+int n = int.Parse(Console.ReadLine());
+CoubOfN(n);
